a13f4168a5991ef85fb5dde3309aebf2f405f9ea
plast7 <plast1206@gmail.com>
2024-10-05 00:04:01 +0900

    fix component name

---
+++ frontend/plast-blog/src/components/NotFoundComponent.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link, createFileRoute } from '@tanstack/react-router'
+import Header from './Header';
+import AnimatedWaveDriver from './AnimatedWaveDivider';
+
+
+const NotFoundComponent = () => {
+  return (
+    <div className="min-h-screen category-page bg-plast-main">
+      <AnimatedWaveDriver />
+      <Header />
+      <main className="pt-40 px-4 max-w-4xl mx-auto">
+        <h1 className="text-[36px] font-bold text-center text-plast-red mb-4">
+          페이지를 찾을 수 없습니다.
+        </h1>
+      </main>
+    </div>
+  );
+};
+
+export default NotFoundComponent;
