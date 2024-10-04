b0bc4acd2dbf8a009951f803976f61ffb23c0102
plast7 <plast1206@gmail.com>
2024-10-05 00:27:20 +0900

    fix front, github action

--- frontend/plast-blog/src/components/RotatingStarsCanvas.tsx
+++ frontend/plast-blog/src/components/RotatingStarsCanvas.tsx
@@ -118,7 +118,7 @@ const RotatingStarsCanvas = () => {
     <div style={{ position: 'relative', width: '100%', height: '100vh', overflow: 'hidden' }}>
       {/* 배경 이미지 */}
       <img
-        src="/public/stella.png"
+        src="/stella.png"
         alt="Background"
         style={{
           position: 'absolute',
